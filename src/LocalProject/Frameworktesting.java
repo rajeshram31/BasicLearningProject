84d6832dcb889708fca06b4d28caa108c84eeacd
Rajesh Ramamurthy <rajeshra@maveric-systems.com>
2021-10-23 11:26:01 +0530

    Sample Git Code Commit

--- src/LocalProject/Frameworktesting.java
+++ src/LocalProject/Frameworktesting.java
@@ -5,8 +5,8 @@ public class Frameworktesting {
 	
 
 	public static void main(String[] args) {
-		System.out.println();
-		
+		System.out.println("This is sample code");
+		System.out.println("Going to commit to Git");
 
 	}
 
