22ce538870dcbc66dc59a68c7aa4b2e3bbe6abfb
Rajesh Ramamurthy <rajeshra@maveric-systems.com>
2021-10-23 11:43:57 +0530

    added new line for pull

--- src/LocalProject/Frameworktesting.java
+++ src/LocalProject/Frameworktesting.java
@@ -7,6 +7,7 @@ public class Frameworktesting {
 	public static void main(String[] args) {
 		System.out.println("This is sample code");
 		System.out.println("Going to commit to Git");
+		System.out.println("going to do some changes and pull it to new branch");
 
 	}
 
